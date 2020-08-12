928e69eb2a3ba00859be51bfee05e08b746202dd
kelvyn14 <kelvynra@gmail.com>
2020-08-12 15:22:48 -0300

    desafios 4

---
+++ curso-vue/dom/desafio-04/app.js
@@ -0,0 +1,51 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		classe1: 'destaque',
+		perigo: true,
+		aplicaDestaque: false,
+		aplicaClasse: '',
+		aplicaInput2Classe: '',
+		input5: '',
+		cor5: '',
+		estilo5: {
+			width: '100px',
+			height: '100px',
+		},	
+		width: '0'
+
+	},
+	computed: {
+		aplicaEx5() {
+			return {
+				inputclasse1: this.input5 == 1,
+				inputclasse2: this.input5 == 2,
+				inputclasse3: this.input5 == 3,
+			}
+
+		}
+	},
+	methods: {
+		iniciarEfeito() {
+			setInterval(() => {
+				this.classe1 = this.classe1 == 'destaque' ? 'encolher' : 'destaque'
+			}, 1000)
+		},
+		iniciarProgresso() {
+		let valor = 0
+		const temporizador = setInterval(() => {
+			valor += 5 
+			this.width = `${valor}%`
+			if(valor == 100) clearInterval(temporizador)
+		},500)
+		},
+		setPerigo(event) {
+			if (event.target.value == 'true') {
+				this.perigo = true
+			} else if (event.target.value == 'false') {
+				this.perigo = false
+			}
+		}
+	}
+
+})
