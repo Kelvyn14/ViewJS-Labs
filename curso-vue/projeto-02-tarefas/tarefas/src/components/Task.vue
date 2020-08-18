402828a60970ab8d25aedeb73edc006c8690f62c
kelvyn14 <kelvynra@gmail.com>
2020-08-18 15:33:08 -0300

    merge

---
+++ curso-vue/projeto-02-tarefas/tarefas/src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tasks"></div>
+</template>
+
+<script>
+export default {
+    props: {
+        task: {
+            type: Object, required: true
+        }
+    }
+}
+</script>
+
+<style >
+
+</style>
